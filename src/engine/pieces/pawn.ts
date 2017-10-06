09b1f5d93eb68324863373e8394bbfa3847bc33a
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 12:39:53 +0100

    made Red-1 work

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,28 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = [];
+        for (let row = 0; row < 8; row++) {
+            for (let col = 0; col < 8; col++) {
+                // let currSquare = new Square(row, col);
+                let currentPiece = board.getPiece(new Square(row, col))
+                if (currentPiece instanceof Pawn){
+                    if (currentPiece.player === Player.WHITE){
+                        moves.push(Square.at(row + 1, col))
+                    }
+                    else{
+                        moves.push(Square.at(row - 1, col))
+                    }
+
+                }
+                // console.log(currentPiece instanceof Queen)
+            }
+        }
+
+
+        // board.findPiece(board.currentPlayer)
+        // let square = Square.at(0, 0)
+        // console.log(board[square.row][square.col])
+        return moves;
     }
 }
