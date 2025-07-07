d814f4c194fac756a47e292245b0a7fccbee7d84
amaliaioanaadetu <Amalia.Adetu@softwire.com>
2025-07-07 16:47:30 +0300

    did red 16

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -3,6 +3,7 @@ import Player from '../player';
 import Board from '../board';
 import Square from "../square";
 import gameSettings from "../gameSettings";
+import King from "./king";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -16,21 +17,29 @@ export default class Knight extends Piece {
                 let currentPiece = board.getPiece(new Square(row, col))
                 if (currentPiece instanceof Knight){
                     if (row + 2 < gameSettings.BOARD_SIZE && col + 1 < gameSettings.BOARD_SIZE)
-                        moves.push(Square.at(row + 2, col + 1))
+                        if ((board.getPiece(new Square(row + 2, col + 1)))?.player !== currentPiece.player && !(board.getPiece(new Square(row + 2,col + 1)) instanceof King))
+                            moves.push(Square.at(row + 2, col + 1))
                     if (row + 2 < gameSettings.BOARD_SIZE && col - 1 >= 0)
-                        moves.push(Square.at(row + 2, col - 1))
+                        if ((board.getPiece(new Square(row + 2, col - 1)))?.player !== currentPiece.player && !(board.getPiece(new Square(row + 2, col - 1)) instanceof King))
+                            moves.push(Square.at(row + 2, col - 1))
                     if (row - 2 >= 0 && col + 1 < gameSettings.BOARD_SIZE)
-                        moves.push(Square.at(row - 2, col + 1))
+                        if ((board.getPiece(new Square(row - 2, col + 1)))?.player !== currentPiece.player && !(board.getPiece(new Square(row - 2, col + 1)) instanceof King))
+                            moves.push(Square.at(row - 2, col + 1))
                     if (row - 2 >= 0 && col - 1 >= 0)
-                        moves.push(Square.at(row - 2, col - 1))
+                        if ((board.getPiece(new Square(row - 2, col - 1)))?.player !== currentPiece.player && !(board.getPiece(new Square(row - 2, col - 1)) instanceof King))
+                            moves.push(Square.at(row - 2, col - 1))
                     if (row + 1 < gameSettings.BOARD_SIZE && col + 2 < gameSettings.BOARD_SIZE)
-                        moves.push(Square.at(row + 1, col + 2))
+                        if ((board.getPiece(new Square(row + 1, col + 2)))?.player !== currentPiece.player && !(board.getPiece(new Square(row + 1, col + 2)) instanceof King))
+                            moves.push(Square.at(row + 1, col + 2))
                     if (row + 1 < gameSettings.BOARD_SIZE && col - 2 >= 0)
-                        moves.push(Square.at(row + 1, col - 2))
+                        if ((board.getPiece(new Square(row + 1, col - 2)))?.player !== currentPiece.player && !(board.getPiece(new Square(row + 1, col - 2)) instanceof King))
+                            moves.push(Square.at(row + 1, col - 2))
                     if (row - 1 >= 0 && col + 2 < gameSettings.BOARD_SIZE)
-                        moves.push(Square.at(row - 1, col + 2))
+                        if ((board.getPiece(new Square(row - 1, col + 2)))?.player !== currentPiece.player && !(board.getPiece(new Square(row - 1, col + 2)) instanceof King))
+                            moves.push(Square.at(row - 1, col + 2))
                     if (row - 1 >= 0 && col - 2 >= 0)
-                        moves.push(Square.at(row - 1, col - 2))
+                        if ((board.getPiece(new Square(row - 1, col - 2)))?.player !== currentPiece.player && !(board.getPiece(new Square(row - 1, col - 2)) instanceof King))
+                            moves.push(Square.at(row - 1, col - 2))
                 }
             }
         }
