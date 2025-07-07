0ad1d5291bb1f88b0c75faf288f558d7ae6070ee
amaliaioanaadetu <Amalia.Adetu@softwire.com>
2025-07-07 14:18:47 +0300

    done red-3

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import gameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,27 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = [];
+        for (let row = 0; row < 8; row++) {
+            for (let col = 0; col < 8; col++) {
+                // let currSquare = new Square(row, col);
+                let currentPiece = board.getPiece(new Square(row, col))
+                if (currentPiece instanceof Rook){
+                    for (let r = 0; r < gameSettings.BOARD_SIZE; r++){
+                        if (r !== row){
+                            moves.push(Square.at(r, col))
+                        }
+                    }
+                    for (let c = 0; c < gameSettings.BOARD_SIZE; c++){
+                        if (c !== col){
+                            moves.push(Square.at(row, c))
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return moves;
     }
 }
