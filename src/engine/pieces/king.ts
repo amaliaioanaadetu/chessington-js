bf46bb6c5e716d4c8b0ee23ec23b9fe134cc24ca
amaliaioanaadetu <Amalia.Adetu@softwire.com>
2025-07-07 14:41:28 +0300

    did red 7

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,24 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = [];
+        for (let row = 0; row < 8; row++) {
+            for (let col = 0; col < 8; col++) {
+                let currentPiece = board.getPiece(new Square(row, col))
+                if (currentPiece instanceof King){
+                    moves.push(Square.at(row + 1, col + 1))
+                    moves.push(Square.at(row + 1, col - 1))
+                    moves.push(Square.at(row - 1, col + 1))
+                    moves.push(Square.at(row - 1, col - 1))
+                    moves.push(Square.at(row, col + 1))
+                    moves.push(Square.at(row, col - 1))
+                    moves.push(Square.at(row + 1, col))
+                    moves.push(Square.at(row - 1, col))
+
+                }
+            }
+        }
+
+        return moves;
     }
 }
