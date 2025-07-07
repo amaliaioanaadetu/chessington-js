e07073a08a3c7e9bd53f161d3dd3c648280ceec8
amaliaioanaadetu <Amalia.Adetu@softwire.com>
2025-07-07 14:38:50 +0300

    did red 6

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import gameSettings from "../gameSettings";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,24 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = [];
+        for (let row = 0; row < 8; row++) {
+            for (let col = 0; col < 8; col++) {
+                let currentPiece = board.getPiece(new Square(row, col))
+                if (currentPiece instanceof Knight){
+                    moves.push(Square.at(row + 2, col + 1))
+                    moves.push(Square.at(row + 2, col - 1))
+                    moves.push(Square.at(row - 2, col + 1))
+                    moves.push(Square.at(row - 2, col - 1))
+
+                    moves.push(Square.at(row + 1, col + 2))
+                    moves.push(Square.at(row + 1, col - 2))
+                    moves.push(Square.at(row - 1, col + 2))
+                    moves.push(Square.at(row - 1, col - 2))
+                }
+            }
+        }
+
+        return moves;
     }
 }
